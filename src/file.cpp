75d7d22240a2ed57dc26fac451d2ce90ca56cca6
Sandra Bastos <sandra@vermillion.net.br>
2019-08-27 16:04:16 -0300

    added implementation for file.h library

---
+++ src/file.cpp
@@ -0,0 +1,38 @@
+#include "file.h"
+
+void writeBPMatrix( std::vector < int > &vector, int puzNum, int rowNum, int colNum )
+{
+	std::ofstream file("bpgMatrix.bp");
+	
+	file << puzNum << std::endl;
+
+	file << rowNum
+		 << colNum
+		 << std::endl;
+
+    for ( int i = 0; i < vector.size(); i++)
+		file << vector[i] << std::endl;
+
+/*	board = *create_Board( &board, rowNum, colNum );
+
+    file << std::endl;
+
+    for( int i = 0; i < rowNum; i++ )
+    {
+        for( int j = 0; j < colNum; j ++ )
+        {
+            file << board.matrix[i][j] << "  ";
+        }
+
+        file << std::endl;
+    }
+*/
+	file.close();
+
+	std::cout << "File saved sucessfully! ;)" << std::endl;
+}
+
+void writeBPArmada()
+{
+
+}
